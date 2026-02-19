2ed17a7a3edc1fffa5a6df211317baebe981db24
temimcojo-byte <temimcojo@gmail.com>
2026-02-19 07:50:01 +0100

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://https://spontaneous-clafoutis-31bb6c.netlify.app"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://spontaneous-clafoutis-31bb6c.netlify.app"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
